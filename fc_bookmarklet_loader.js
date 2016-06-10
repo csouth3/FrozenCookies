6858a3b6d5b449f9e101f139a72e0e784307807f
Chase Southwood <csouth3@illinois.edu>
2016-06-09 22:54:51 -0500

    Point to my github page

--- fc_bookmarklet_loader.js
+++ fc_bookmarklet_loader.js
@@ -3,7 +3,7 @@ javascript:(
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
     js.setAttribute('id', 'frozenCookieScript');
-    js.setAttribute('src', 'http://icehawk78.github.io/FrozenCookies/frozen_cookies.js');
+    js.setAttribute('src', 'http://csouth3.github.io/FrozenCookies/frozen_cookies.js');
     document.head.appendChild(js);
  }()
 );
